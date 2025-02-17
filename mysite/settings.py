897d292980b07e1c6025163c950046d35a6fe3b1
KIMKIMzamasan <czarssama@gmail.com>
2025-02-17 13:41:31 +0800

    customized

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',  # Add this line
+    
 ]
 
 MIDDLEWARE = [
@@ -115,6 +116,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
